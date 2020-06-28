52b3dd08f5c680b4ba8d8fe9bb1d988c73f79793
Santiago Rincon <santiagorincon2001@gmail.com>
2020-06-28 13:28:12 -0500

    Se actualiza el footer

--- js/efectos.js
+++ js/efectos.js
@@ -1,12 +1,5 @@
 
 $(function () {
-    /*Rotacion ficha*/
-    $(function(){
-      document.querySelector(".card-flip").classList.toggle("flip");
-      $('.sidenav').sidenav();
-    });
-
-
     /*Redirecciones*/
     $("#btnVolverPaginaPrincipal").click(function(){
       window.location="index.html";
@@ -15,3 +8,53 @@ $(function () {
       window.location="index.html";
     });
 });
+
+$(function(){
+  /*Validaciones js formulario contacto*/
+  //setup before functions
+  var contador;                //timer identifier
+  var tiempoValidacion = 300;  //300 ms
+
+  /*Al dejar de presionar una tecla empieza el contador*/
+  $('#btnContactoNombre').keyup(function(){
+      clearTimeout(contador);
+      if ($('#btnContactoNombre').val()) {
+          contador = setTimeout(validarContactoNombre, tiempoValidacion);
+      }
+  });
+  $('#btnContactoCorreo').keyup(function(){
+      clearTimeout(contador);
+      if ($('#btnContactoCorreo').val()) {
+          contador = setTimeout(validarContactoEmail, tiempoValidacion);
+      }
+  });
+  $('#btnContactoInteres').keyup(function(){
+      clearTimeout(contador);
+      if ($('#btnContactoInteres').val()) {
+          contador = setTimeout(validarContactoInteres, tiempoValidacion);
+      }
+  });
+});
+
+/*Validacion logica de los campos*/
+function validarContactoNombre () {
+    $("#btnContactoNombre").removeClass("is-valid is-invalid").addClass("is-valid");
+}
+function validarContactoEmail () {
+    if(validateEmail($("#btnContactoCorreo").val())){
+      $("#btnContactoCorreo").removeClass("is-valid is-invalid").addClass("is-valid");
+    }else{
+      $("#btnContactoCorreo").removeClass("is-valid is-invalid").addClass("is-invalid");
+    }
+  }
+
+function validarContactoInteres () {
+    $("#btnContactoInteres").removeClass("is-valid is-invalid").addClass("is-valid");
+}
+
+/* Fuente: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript*/
+function validateEmail(email)
+    {
+        var re = /\S+@\S+\.\S+/;
+        return re.test(email);
+    }
